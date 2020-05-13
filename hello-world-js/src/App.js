a7ea0b29a4e7b346fb390b89ac3eb81d7d281883
Marieta <maria.it3@gmail.com>
2020-05-13 18:56:02 +0200

    Segundo commit

---
+++ hello-world-js/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Greetings from './components/Greetings/Greetings'
+
+function App() {
+  return (
+    <div className="App">
+      <Greetings name={'Marieta'}> </Greetings>
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world-js/src/components/Greetings/Greetings.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Greetings ({name}){
+    return <div>Hola {name}</div>
+}
+
+export default Greetings
